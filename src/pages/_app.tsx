368a270bf4a4b550c7725ef0eff2539064660fec
Washington Campos <washingtoncampos9@gmail.com>
2021-08-27 13:21:34 -0300

    chore: set up tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "tailwindcss/tailwind.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		container: {
+			center: true,
+		},
+		fontFamily: {
+			sans: ["Graphik", "sans-serif"],
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
