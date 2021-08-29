e93490c76fd24805bac1b6973bc53f54d91f6d87
Washington Campos <washingtoncampos9@gmail.com>
2021-08-28 22:57:40 -0300

    feat: create TopBar component

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,11 @@
+const TopBar = () => {
+	return (
+		<div className="w-full h-10 flex items-center bg-gray-400">
+			<span className="block w-4 h-4 mx-1 rounded-full bg-gray-500" />
+			<span className="block w-4 h-4 mx-1 rounded-full bg-gray-500" />
+			<span className="block w-4 h-4 mx-1 rounded-full bg-gray-500" />
+		</div>
+	);
+};
+
+export default TopBar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,19 @@ import { queries } from "services/queries";
 import { UserProps } from "types/user";
 
 import Main from "components/Main";
+import TopBar from "components/TopBar";
 
 type HomeProps = {
 	users: UserProps[];
 };
 
 const Home = ({ users }: HomeProps) => {
-	return <Main users={users} />;
+	return (
+		<>
+			<TopBar />
+			<Main users={users} />
+		</>
+	);
 };
 
 export const getStaticProps = async () => {
